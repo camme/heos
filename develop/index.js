c2777ab506925ec72e6443284df705c065733bca
camilo tapia <camilo.tapia@gmail.com>
2015-07-27 00:49:06 +0200

    tested some more and added playnext

--- develop/index.js
+++ develop/index.js
@@ -5,10 +5,27 @@ heos.connect()
         console.log('Connected');
         return client.getPlayers()
             .then(function(players) {
-                console.log("Players", players);
-                return players[0].getNowPlaying()
+                console.log("Players:");
+                players.forEach(function(player) {
+                    console.log(player.name);
+                });
+                var player1 = players[0];
+                return player1.getPlayState()
+                    .then(function(state) {
+                        console.log('State:', state);
+                    })
+                    .then(function() {
+                        return player1.getNowPlaying()
+                            .then(function(media) {
+                                if (!media.song) {
+                                    return player1.getNowPlaying();
+                                }  
+                                return media;
+                            });
+                    })
                     .then(function(playing) {
-                        console.log('Now playing ', playing);
+                        console.log('Now playing:', playing);
+                        //return player1.playNext();
                     });
             });
     })
